7d5387e10e94280dee62fa0d8a782c8338060130
BryceVerberne <bryceverberne@gmail.com>
2023-03-17 14:05:02 -0700

    :white_check_mark: Complete Procedure Calls lab

--- Procedures/Procedure Calls Lab.asm
+++ Procedures/Procedure Calls Lab.asm
@@ -25,21 +25,53 @@
 .text
   .globl main
 main:
-  # Type your code here.
+  li $s0, 5   # Load $a0 with 5 (our x variable)
+  li $s1, 10  # Load $a1 with 10 (our y variable)
+      
+  # Load our arguments
+  add $a0, $zero, $s0
+  add $a1, $zero, $s1
+      
+      
+  # Jump to Sum procedure and store return value in $s2
+  jal Sum
+  move $s2, $v0
   
-   
+  # Print value of $s2
+  move $a0, $s2
+  li   $v0, 1
+  syscall
+  
+  li $a0, '\n'
+  li $v0, 11
+  syscall
+  
+  
+  # Load our arguments
+  add $a0, $zero, $s1
+  add $a1, $zero, $s0
+  
+  # Jump to Dif procedure and store return value in $s3
+  jal Dif
+  move $s3, $v0
+  
+  move $a0, $s3
+  li   $v0, 1
+  syscall
+      
+      
   # Terminate Program
   li $v0, 10
-  syscall 
-      
+  syscall
+  
 
 # Procedure Sum (Do not modify)
 Sum:
   add $v0, $a0, $a1
-  jr $ra
+  jr  $ra
 
 
 # Procedure Dif (Do not modify)
 Dif:
   sub $v0, $a0, $a1
-  jr $ra
+  jr  $ra
