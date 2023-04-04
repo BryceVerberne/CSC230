453a812977a86d93f1370b71ee97f2331752a551
BryceVerberne <bryceverberne@gmail.com>
2023-04-04 14:16:54 -0700

    Add Array Basics tutorial

---
+++ Arrays/Array Basics/Array Basics.asm
@@ -0,0 +1,22 @@
+# Title:  Array Basics
+# Desc:   This program demonstrates how to make arrays
+# Author: Bryce Verberne
+# Date:   04/04/2023
+
+
+
+# Info:
+#  - An array is a collection of data. 
+#  - An array is stored in RAM, where each one of its values are stored next to each other. 
+
+.data
+
+
+.text
+  .globl main
+main:
+
+
+  # Terminating Program
+  li $v0, 10
+  syscall
