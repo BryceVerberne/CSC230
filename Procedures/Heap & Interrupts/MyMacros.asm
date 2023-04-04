b178537226ac2c2cd9d75c05ec4bd52902489e90
BryceVerberne <bryceverberne@gmail.com>
2023-04-04 11:05:49 -0700

    ✨ Add malloc  to MyMacros.asm

--- Procedures/Heap & Interrupts/MyMacros.asm
+++ Procedures/Heap & Interrupts/MyMacros.asm
@@ -12,7 +12,7 @@
 .end_macro
    
 .macro readInt (%t)
-  li $v0, 5   # The code to read an int is 5. 
+  li $v0, 5     # The code to read an int is 5. 
   syscall
    
   move %t, $v0
@@ -33,4 +33,13 @@
   li $a0, '\n'
   li $v0, 11
   syscall
+.end_macro
+
+# Allocate $t bytes of memory & store resulting base address in %v
+.macro malloc (%t) (%v)
+  move $a0, %t  # Move the # of byets to allocate into $a0
+  li   $v0, 9
+  syscall
+  
+  move %v, $v0  # Move the resulting address into %v
 .end_macro
