7b1fcb3dd5070989d220045c498f2b01cb6423ca
BryceVerberne <bryceverberne@gmail.com>
2023-04-04 10:25:46 -0700

    Include the MyMacros.asm file

--- Procedures/Heap & Interrupts/Heap & Interrupts.asm
+++ Procedures/Heap & Interrupts/Heap & Interrupts.asm
@@ -3,11 +3,15 @@
 # Author: Bryce Verberne
 # Date:   04/04/2023
 
+
+# We want to ask the user how many random #'s they want, & then generate those random #'s in an array. 
+
 .data
 
 .text
   .globl main
 main:
+  
   
   
   # Terminating Program
