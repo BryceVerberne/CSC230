1342a06e8c1076570cfbe464be63eaaf7761671b
BryceVerberne <bryceverberne@gmail.com>
2023-04-04 13:22:16 -0700

    Add User Input tutorial

---
+++ Input & Output/User Input/User Input.asm
@@ -0,0 +1,18 @@
+# Title:  User Input
+# Desc:   This program demonstrates how to take input from the user.
+# Author: Bryce Verberne
+# Date:   04/04/2023
+
+
+
+.data
+
+
+.text
+  .globl main
+main:
+
+
+  # Terminating Program
+  li $v0, 10
+  syscall
