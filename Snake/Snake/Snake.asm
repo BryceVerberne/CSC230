f072601e8590ca500494bd30c9ae4db8f4b4c217
BryceVerberne <bryceverberne@gmail.com>
2023-04-11 10:06:22 -0700

    Add assembly Snake program

---
+++ Snake/Snake/Snake.asm
@@ -0,0 +1,18 @@
+# Title:  Snake
+# Desc:   This program emulates the game Snake 
+# Author: Bryce Verberne
+# Date:   04/11/2023
+
+
+
+.data
+
+
+.text
+  .globl main
+main:
+  
+  
+  # Terminate Program
+  li $v0, 10
+  syscall
