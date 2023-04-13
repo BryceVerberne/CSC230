a68eba246a6780207abc2c77eaa72e9313295179
BryceVerberne <bryceverberne@gmail.com>
2023-04-13 13:27:39 -0700

    🔁 Add a nested loop to iterate from 99 to 0

--- Display/CountDown/CountDown.asm
+++ Display/CountDown/CountDown.asm
@@ -1,66 +1,88 @@
 # Title:  Count Down
-# Desc:   This program displays a countdown from 99 to 1 on a seven-segment display
+# Desc:   This program displays a countdown from 99 to 0 on a seven-segment display
 # Author: Bryce Verberne
 # Title:  04/12/2023
 
 
 
-# This tool is composed of 3 parts : 
+# Tool Composition: 
 #  1. Two seven-segment displays
 #  2. A hexadecimal keyboard
 #  3. A counter 
 
-.include "MyMacros.asm"
-
-#  Seven segment display
-#  Byte value at address 0xFFFF0010 : command right seven segment display 
-#  Byte value at address 0xFFFF0011 : command left seven segment display 
-#  Each bit of these two bytes are connected to segments (bit 0 for a segment, 1 for b segment and 7 for point 
-.eqv SEG1 0xFFFF0010
-.eqv SEG2 0xFFFF0011
+#  Seven segment display:
+#   - Byte value at address 0xFFFF0010 : command right seven segment display 
+#   - Byte value at address 0xFFFF0011 : command left seven segment display 
+#     - Each bit of these two bytes are connected to segments
+.eqv R_SEG 0xFFFF0010
+.eqv L_SEG 0xFFFF0011
 
 # Create number segments 0-9
-.eqv ZERO  0x3f  # Binary: 0011 1111
-.eqv ONE   0x06  # Binary: 0000 0110
-.eqv TWO   0x5b  # Binary: 0101 1011
-.eqv THREE 0x4f  # Binary: 0100 1111
-.eqv FOUR  0x66  # Binary: 0110 0110
-.eqv FIVE  0x6d  # Binary: 0110 1101
-.eqv SIX   0x7d  # Binary: 0111 1101
-.eqv SEVEN 0x07  # Binary: 0000 0111
-.eqv EIGHT 0x7f  # Binary: 0111 1111
-.eqv NINE  0x6f  # Binary: 0110 1111
+.eqv ZERO  0x3f
+.eqv ONE   0x06
+.eqv TWO   0x5b
+.eqv THREE 0x4f
+.eqv FOUR  0x66
+.eqv FIVE  0x6d
+.eqv SIX   0x7d
+.eqv SEVEN 0x07
+.eqv EIGHT 0x7f
+.eqv NINE  0x6f
+
+.include "MyMacros.asm"
 
 .data
-  nums: .byte ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+  nums: .byte NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO, ONE, ZERO
   
 .text
  .globl main
 main:
-  addi $s0, $zero, SEG1   # $s0 now holds the address of segment 1
-  addi $s1, $zero, SEG2   # $s1 now holds the address of segment 2
+  addi $s0, $zero, R_SEG    # $s0 now holds the address of segment 1
+  addi $s1, $zero, L_SEG    # $s1 now holds the address of segment 2
+   
+  # Base of our list of numbers for tensLoop & onesLoop
+  la $s2, nums
+  la $s3, nums
   
-  la $s2, nums            # Base of our list of numbers
+  # Set the loop stopping value
+  addi $s4, $zero, 10
   
-  add $t0, $zero, $zero   # Iterator (++i)
-  addi $s3, $zero, 10     # Loop stopping value
-  
-  # Number Segment tester
-  forLoop:
+  # This loop displays to the L_SEG (tens place)
+  tensLoop:
     # Condition 
-    beq $t0, $s3, endLoop # If $t0 = $s0 end the loop
+    beq $t0, $s4, endTens   # If $t0 = $s4 (i=10) end the loop
     
     # Logic
-    lb $t0, 0($s2)        # Load ZERO into $t0
-    sb $t0, 0($s0)        # Write it to segment 1
-    sleep
+    lb $t2, 0($s2)          # Load the current number segment into $t2
+    sb $t2, 0($s1)          # Write $t2 to L_SEG
     
-    # Prep
-    addi $t0, $t0, 1      # Iterate $t0
-    addi $s2, $s2, 1      # Cycle through the array
+    # Reset nums address & iterator for onesLoop
+    la $s3, nums
+    li $t1, 0
     
-    b forLoop
-  endLoop:
+    # This loop displays to the R_SEG (ones place)
+    onesLoop:
+     # Condition 
+     beq $t1, $s4, endOnes  # If $t1 == $s4 (j=10) end the loop 
+     
+     # Logic
+     lb $t3, 0($s3)         # Load the current number segment into $t3
+     sb $t3, 0($s0)         # Write $t3 to R_SEG
+     sleep
+     
+     # Prep for next iteration
+     addi $t1, $t1, 1       # Iterate $t1 (++j)
+     addi $s3, $s3, 1       # Cycle through the array
+     
+     b onesLoop
+   endOnes:
+    
+    # Prep for next iteration
+    addi $t0, $t0, 1        # Iterate $t2 (++i)
+    addi $s2, $s2, 1        # Cycle through the array
+    
+    b tensLoop
+  endTens:
   
     
   # Terminate Program  
